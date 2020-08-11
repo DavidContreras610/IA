9fc51f5753295c04ece47bd7241461c4acbf0a5c
DavidContreras610 <dvdcontrrs@gmail.com>
2020-08-10 23:51:40 -0700

    finished mvp

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -3,18 +3,37 @@ import { NgModule } from '@angular/core';
  import { FormsModule } from '@angular/forms';
 
  import { IonicModule } from '@ionic/angular';
-
+ import { ProfilePage } from './profile.page';
+ import { RouterModule } from '@angular/router';
+ import { RatingComponent } from '../rating/rating.component';
  import { ProfilePageRoutingModule } from './profile-routing.module';
 
- import { ProfilePage } from './profile.page';
 
  @NgModule({
    imports: [
      CommonModule,
      FormsModule,
      IonicModule,
+     RouterModule.forChild([{ path: '', component: ProfilePage }]),
      ProfilePageRoutingModule
    ],
-   declarations: [ProfilePage]
+   declarations: [ProfilePage, RatingComponent]
  })
  export class ProfilePageModule {}
+
+
+//  import { RatingComponent } from '../rating/rating.component';
+//  import { Tab3PageRoutingModule } from './tab3-routing.module';
+
+// @NgModule({
+//   imports: [
+//     IonicModule,
+//     CommonModule,
+//     FormsModule,
+//     ExploreContainerComponentModule,
+//     RouterModule.forChild([{ path: '', component: Tab3Page }]),
+//     Tab3PageRoutingModule,
+//   ],
+//   declarations: [Tab3Page, RatingComponent]
+// })
+// export class Tab3PageModule {}
